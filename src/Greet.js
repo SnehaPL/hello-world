daffdcae69459a00ace912b9f1b41ca7d9760297
SnehaPatilBirje <47850031+SnehaPatilBirje@users.noreply.github.com>
2019-05-03 16:25:29 +0530

    Learnign JSX Props

--- src/Greet.js
+++ src/Greet.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-function Greet(){
-    return <h1> Functional Components </h1>
+function Greet(props){
+    console.log(props);
+    return (
+        <div>
+            <h1> Hello {props.name} {props.surname} </h1>
+            {props.children}
+        </div>
+    )
 }
 
 export default Greet
