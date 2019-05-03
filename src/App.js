0adee00709b5b4d38d57e13eef9b6374546c5799
SnehaPatilBirje <47850031+SnehaPatilBirje@users.noreply.github.com>
2019-05-03 18:39:11 +0530

    Learning setStates

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 import './App.css';
 import Greet from './component/Greet.js';
 import Welcome from './component/Welcome.js';
+import Hello from './component/Hello.js';
+import Message from './component/Message.js';
+import Counter from './component/Counter.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
     <h1>Hello World!</h1>
-     <Greet/>
-     <Welcome/>
-      </header>
+     <Greet name="Asmi" surname="Birje">
+      <p>
+        Age 2 years
+      </p>
+     </Greet>
+     <Greet name="Riya" surname="Sawant">
+      <p>Age 6 years</p>
+     </Greet>
+     <Welcome name="Jai" surname="Sawant"/>
+     <Hello/>
+     <Message/>
+     <Counter/>
     </div>
   );
 }
